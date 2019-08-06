863066a141dd3f957518dfb576d2370bd4c3672b
Vikas sangwan <vickysangwan@protonmail.com>
2019-08-07 00:19:47 +0530

    Blog added

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'job.apps.JobConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
