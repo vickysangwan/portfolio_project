a604592e9ab52c9640f5cc19cbc7aef6937815cd
Vikas sangwan <vickysangwan@protonmail.com>
2019-08-07 23:42:47 +0530

    Job is done

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import job.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',job.views.home, name='home'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
